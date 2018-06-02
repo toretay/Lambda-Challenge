43cd625859e08e484b0452f4d74d1171a2a63818
toretay <toretay@gmail.com>
2018-06-02 19:07:37 +0500

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+	return 'Hello World!';
 }
 
 /*
@@ -23,8 +23,16 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+	if (num % 3 === 0 && num % 5 === 0) {
+		return "Lambda School";
+	} else if (num % 3 === 0) {
+		return "Lambda";
+	} else if (num % 5 === 0) {
+		return "School";
+	} else {
+		return num;
+	}
 }
 
 /*
@@ -38,8 +46,16 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
+function longestString(strs) {
+	var strLength = 0,
+		str;
+	for (var i = 0; i < strs.length; i++) {
+		if (strLength < strs[i].length) {
+			strLength = strs[i].length;
+			str = strs[i];
+		}
+	}
+	return str;
 }
 
 /*
@@ -63,8 +79,13 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
-
+function computeUserAverageAge(users) {
+	var age = 0;
+	for (var i = 0; i < users.length; i++) {
+		var usersAge = users[i].age;
+	    age = age + usersAge;
+	}
+  	return Math.round(age / users.length);
 }
 
 module.exports = {
